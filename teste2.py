7a00e42a14712deb0111d68de9f8dc4da6d1dc69
luan <luaanliima@live.com>
2022-09-21 15:26:32 -0300

    commit inicial

---
+++ teste2.py
@@ -0,0 +1,14 @@
+def nota_minima(notas):
+    quantidade = len(notas)
+    soma = sum(notas)
+    media = soma / quantidade
+    print ("O aluno precisa de" media "para ser aprovado")
+    verificar_nota(media)
+
+def verificar_nota(media):
+    if media >=6
+    print ("Aluno aprovado :) ")
+
+nota1 = input("informe a primeira nota")
+nota2 = input("informe a segunda nota")
+    
