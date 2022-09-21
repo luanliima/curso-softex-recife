833dd6a8605c39e00b8876ce7c618be1a8536d05
luan <luaanliima@live.com>
2022-09-21 16:47:36 -0300

    modificação de código

--- teste2.py
+++ teste2.py
@@ -1,14 +1,7 @@
-def nota_minima(notas):
-    quantidade = len(notas)
-    soma = sum(notas)
-    media = soma / quantidade
-    print ("O aluno precisa de" media "para ser aprovado")
-    verificar_nota(media)
 
-def verificar_nota(media):
-    if media >=6
-    print ("Aluno aprovado :) ")
-
-nota1 = input("informe a primeira nota")
-nota2 = input("informe a segunda nota")
+nota1 = float(input("informe a primeira nota : "))
+nota2 = float(input("informe a segunda nota : "))
+nota3 = 21 - nota1 - nota2
+print (f"voçê precisa de {nota3} para ser aprovado")
+   
     
