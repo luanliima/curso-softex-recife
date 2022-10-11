d073d8a996f4b048f027ebdd1fe6f82efd02409c
luan <luaanliima@live.com>
2022-10-11 11:03:57 -0300

    banco.commit

--- modulo 2/nivel 4/ATIV01_NIV04.py
+++ modulo 2/nivel 4/ATIV01_NIV04.py
@@ -1,75 +1,75 @@
-Conta classe:
-    limiteChequeEspecial = 1000,0
+class Conta:
+    limiteChequeEspecial  = 1000.0
 
-    def __init__ (self, numero, saldo):
-        auto. numero = numero
-        auto.  saldo = saldo
+    def __init__(self, numero, saldo):
+        self.numero = numero
+        self.saldo = saldo
 
     def __str__(self):
-        retorno f"Conta {self. numero}: R$ {self. saldo}. Limite: {self. limiteChequeEspecial})"
+        return f"Conta {self.numero}: R$ {self.saldo}. Limite: {self.limiteChequeEspecial})"
 
-    debitar def (self, valor):
-        auto. saldo = self. saldo - valor
+    def debitar(self, valor):
+        self.saldo = self.saldo - valor
 
-    def creditar (self, valor):
-        auto. saldo = self. saldo + valor
+    def creditar(self, valor):
+        self.saldo = self.saldo + valor
 
-    def modificadoLimite(valor):
-        se valor > Conta. limiteChequeEspecial:
-            Conta, o que está por fazer. limiteChequeEspecial = valor
+    def modificarLimite( valor):
+        if valor > Conta.limiteChequeEspecial:
+            Conta.limiteChequeEspecial = valor
 
-classe Banco:
+class Banco:
     def __init__(self):
-        auto. contas = {}
+        self.contas = {}
 
-    def cadastrarConta (self, conta):
-        auto. contas [conta. numero] = conta
+    def cadastrarConta(self, conta):
+        self.contas[conta.numero] = conta
 
     def relatorioContas(self):
-        impressão (self. contas)
+        print(self.contas)
 
-    def transferencia (self, numeroOrigem, numeroDestino, valor):
-        contaOrigem = self. contas[numeroOrigem]
-        constestino = self. contas[numeroDestino]
+    def transferencia(self, numeroOrigem, numeroDestino, valor):
+        contaOrigem = self.contas[numeroOrigem]
+        contaDestino = self.contas[numeroDestino]
         
-        se contaOrigem. saldo >= valor:
-            contaOrigem. débito (valor)
-            contaDestino. creditar (valor)
-            impressão("Valor transferido: ", valor)
-        outra coisa:
-            se abs (contaOrigem. saldo - valor) <= contaOrigem. limiteChequeEspecial:
-                contaOrigem. débito (valor)
-                contaDestino. creditar (valor)
-                impressão("Valor transferido: ", valor)
-            outra coisa:
-                imprimir(f"A conta {numeroOrigem} excedeu o limite do cheque especial.")
+        if contaOrigem.saldo >= valor:
+            contaOrigem.debitar(valor)
+            contaDestino.creditar(valor)
+            print("Valor transferido: ", valor)
+        else:
+            if abs(contaOrigem.saldo - valor) <= contaOrigem.limiteChequeEspecial:
+                contaOrigem.debitar(valor)
+                contaDestino.creditar(valor)
+                print("Valor transferido: ", valor)
+            else:
+                print(f"A conta {numeroOrigem} excedeu o limite do cheque especial.")
             
 
-c1 = Conta("123-4", 100,0)
-c2 = Conta("678-9",500,0)
+c1 = Conta("123-4", 100.0)
+c2 = Conta("678-9", 500.0)
 
-Conta, o que está por fazer. modificarLimite(2.0)
+Conta.modificarLimite(2.0)
 
-impressão(c1)
-imprimir(c2)
+print(c1)
+print(c2)
 
 bancoDoBrasil = Banco()
 caixa = Banco()
 
-#bancoDoBrasil.relatorioContas
+#bancoDoBrasil.relatorioContas()
 
-bancoDoBrasil. cadastrarConta(c1))
+bancoDoBrasil.cadastrarConta(c1)
 
-Caixa. cadastrarConta(c2))
+caixa.cadastrarConta(c2)
 
-#bancoDoBrasil.relatorioContas
-bancoDoBrasil. transferencia ("123-4", "678-9" , 10000,0)
+#bancoDoBrasil.relatorioContas()
+bancoDoBrasil.transferencia("123-4", "678-9", 10000.0)
 
-bancoDoBrasil. transferencia ("123-4", "678-9", 1.0)
-bancoDoBrasil. transferencia ("123-4", "678-9", 50.0)
-bancoDoBrasil. transferencia ("123-4", "678-9", 100.0)
-bancoDoBrasil. transferencia ("123-4", "678-9", 10.0)
-bancoDoBrasil. transferencia ("123-4", "678-9" , 1000,0)
+bancoDoBrasil.transferencia("123-4", "678-9",  1.0)
+bancoDoBrasil.transferencia("123-4", "678-9",  50.0)
+bancoDoBrasil.transferencia("123-4", "678-9",  100.0)
+bancoDoBrasil.transferencia("123-4", "678-9",  10.0)
+bancoDoBrasil.transferencia("123-4", "678-9",  1000.0)
 
-impressão(c1)
-impressão(c2)
+print(c1)
+print(c2)
